786bbe470ac6e03c3d8aff2c8f2aab859714dc12
Anthony Torres <54015024+HazelnutBoi@users.noreply.github.com>
2025-09-08 04:18:49 -0500

    Refactor Firebase initialization and use env variable

--- api/apitrack-view.js
+++ api/apitrack-view.js
@@ -1,25 +1,25 @@
 // api/track-view.js
-
+    
 const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
+    
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://menu-cafeteria-crpo-default-rtdb.firebaseio.com"
+    databaseURL: process.env.FIREBASE_DATABASE_URL
   });
 }
-
+    
 const db = admin.database();
-
+    
 module.exports = async (req, res) => {
   try {
-    const date = new Date().toISOString().split('T')[0]; // Formato YYYY-MM-DD
+    const date = new Date().toISOString().split('T')[0];
     const viewsRef = db.ref(`analytics/views/${date}`);
     await viewsRef.transaction((currentValue) => {
       return (currentValue || 0) + 1;
     });
-
+        
     return res.status(200).send({ message: "Vista registrada" });
   } catch (error) {
     console.error("Error al registrar la vista:", error);
